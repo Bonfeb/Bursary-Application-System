b3a63b766740e15eeff24a5f17dd640aac65ed5f
Bonfeb <bonfebdevs@gmail.com>
2024-05-15 23:41:06 +0300

    anonymous

--- KCBAWS/settings.py
+++ KCBAWS/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL = os.environ.get('DB_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
