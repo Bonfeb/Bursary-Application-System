28cc5e88822a0e8997c328717529ca46fd58b0a4
Bonfeb <bonfebdevs@gmail.com>
2024-05-16 00:15:48 +0300

    anonymous

--- KCBAWS/settings.py
+++ KCBAWS/settings.py
@@ -163,7 +163,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL  = config('DEFAULT_FROM_EMAIL')
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
